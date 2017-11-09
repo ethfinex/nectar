74318f8b87ab150293826a10a38ca90bd46f9a7d
Will Harborne <will.harborne@gmail.com>
2017-11-09 21:29:25 +0000

    Deployment comments

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,9 +2,10 @@ var MiniMeTokenFactory = artifacts.require('MiniMeTokenFactory')
 var NEC = artifacts.require('./NEC.sol')
 var NectarController = artifacts.require('./NectarController.sol')
 
-// This should probably be the same as Tether Multisig
+// Cold Storage
 var efxVaultWallet = '0x66f3edf3865b9830911614462d5fd81a0a798808'
 
+// This is the order required to deploy the contracts. The controller of NEC is later set as the NectarController contract
 module.exports = function (deployer) {
   deployer.deploy(MiniMeTokenFactory)
     .then(function () {
